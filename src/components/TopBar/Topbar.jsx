aed0ae77ff52738ffb9046be298dd9a18a97f5b7
Balaguru S <balagurusiva2002@gmail.com>
2023-12-19 08:41:24 +0530

    modified:   index.css 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/TopBar/Topbar.jsx 	modified:   src/components/TopBar/topbar.css 	modified:   tailwind.config.js

--- src/components/TopBar/Topbar.jsx
+++ src/components/TopBar/Topbar.jsx
@@ -1,21 +1,23 @@
 import { Search, Person, Chat, Notifications, } from "@mui/icons-material"
+import './topbar.css'
 
 const Topbar = () => {
     return (
-        <div className="topbar-container h-[50px] w-[100%] bg-[#ffffff]">
-            <div className="topbar-left">Shareify</div>
+        <div className=" topbar-container  ">
+            <div className="topbar-left">
+                <span className=" logo  ">Shareify</span></div>
             <div className="topbar-center">
                 <div className="searchbar">
-                    <Search />
+                    <Search className="search-icon"/>
                     <input type="text" placeholder="Search for friends, posts or video" className="searchbar-input" />
                 </div>
             </div>
             <div className="topbar-right">
-                <div className="topbar-links">
-                    <span className="topbars=link">Home</span>
-                    <span className="topbar-link">Timeline</span>
+                <div className="topbar-links ">
+                    <div className="topbar-link">Home</div>
+                    <div className="topbar-link">Timeline</div>
                 </div>
-                <div className="topbar-icon">
+                <div className="topbar-icon ">
                     <div className="topbar-icon-item">
                         <Person />
                         <span className="topbar-icon-badge">1</span>
@@ -29,7 +31,7 @@ const Topbar = () => {
                         <span className="topbar-icon-badge">1</span>
                     </div>
                 </div>
-                <img src="public\assets\person\bala.jpg" alt="" className="topbar-img" />
+                <img src="public\assets\person\bala.jpg" alt="profile img" className="topbar-img" />
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        flex:{
+            '2':'2 2 0%',
+            '3':'3 3 0%',
+            '4':'4 4 0%',
+            '5':'5 5 0%',
+            '6':'6 6 0%'
+        }
+    },
   },
   plugins: [],
 }
-
