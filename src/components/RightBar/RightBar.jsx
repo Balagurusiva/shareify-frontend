017973a42b8cd1495204b44654294591f98819b2
Balaguru S <balagurusiva2002@gmail.com>
2023-12-23 09:19:27 +0530

    modified:   src/App.jsx 	modified:   src/components/RightBar/RightBar.jsx 	modified:   src/components/RightBar/rightbar.css 	new file:   src/pages/profile/Profile.jsx 	new file:   src/pages/profile/profile.css 	modified:   tailwind.config.js

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -1,30 +1,38 @@
 import './rightbar.css'
 
-const RightBar = () => {
-  return (
-     <div className="rightbar">
-        <div className="rightbar-wrapper">
+const RightBar = ({ profile }) => {
+
+    const HomeRightbar = () => (
+        <>
             <h3 className="rightbar-title">Online Friends  </h3>
-                <ul className="rightbar-friend-list">
-                    <li className="rightbar-friend">
-                        <div className="rightbar-profile-img-container">
-                            <img src="public\assets\person\bala.jpg" alt="" className="rightbar-img" />
-                            <span className="rightbar-online"></span>
-                        </div>
-                        <span className="rightbar-user-name">Bala</span>
-                    </li>
-                    <li className="rightbar-friend">
-                        <div className="rightbar-profile-img-container">
-                            <img src="public\assets\person\bala.jpg" alt="" className="rightbar-img" />
-                            <span className="rightbar-online"></span>
-                        </div>
-                        <span className="rightbar-user-name">Bala</span>
-                    </li>
-                    
-                </ul>
+            <ul className="rightbar-friend-list">
+                <li className="rightbar-friend">
+                    <div className="rightbar-profile-img-container">
+                        <img src="public\assets\person\bala.jpg" alt="" className="rightbar-img" />
+                        <span className="rightbar-online"></span>
+                    </div>
+                    <span className="rightbar-user-name">Bala</span>
+                </li>
+                <li className="rightbar-friend">
+                    <div className="rightbar-profile-img-container">
+                        <img src="public\assets\person\bala.jpg" alt="" className="rightbar-img" />
+                        <span className="rightbar-online"></span>
+                    </div>
+                    <span className="rightbar-user-name">Bala</span>
+                </li>
+
+            </ul>
+        </>
+    )
+
+    const profilrRightbar = () => (<div>profile</div>)
+    return (
+        <div className="rightbar">
+            <div className="rightbar-wrapper">
+                {!profile ? HomeRightbar() : profilrRightbar()}
+            </div>
         </div>
-     </div>
-  )
+    )
 }
 
 export default RightBar
---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,38 @@
+ import './profile.css'
+import Topbar from '../../components/TopBar/Topbar'
+import RightBar from '../../components/RightBar/RightBar'
+import Feed from '../../components/Feed/Feed'
+import Sidebar from '../../components/Sidebar/sidebar'
+
+const Profile = () => {
+  return (
+     <>
+            <Topbar />
+            <div className="profile">
+             
+                    <Sidebar />
+             
+                 
+                 <div className="profile-right">
+                    <div className="profile-right-top">
+                        <div className="profile-cover">
+                            <img src="public\assets\person\bala.jpg" alt="" className="profile-cover-img" />
+                            <img src="public\assets\person\bala.jpg" alt="" className="profile-user-img" />
+                        </div>
+                        <div className="profile-info">
+                            <h4 className='profile-info-name'>Balaguru S</h4>
+                            <span className="profile-info-desc">Helllo prnds</span>
+                        </div>
+                    </div>
+                    <div className="profile-right-bottom">
+                        <Feed />
+                        <RightBar profile />
+                    </div>
+                 </div>
+                 
+            </div>
+        </>
+  )
+}
+
+export default Profile
