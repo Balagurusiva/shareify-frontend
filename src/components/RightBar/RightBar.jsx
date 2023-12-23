eef23af4740fe85ad26ad7e12ef42b95a592fe5a
Balaguru S <balagurusiva2002@gmail.com>
2023-12-23 08:17:08 +0530

    modified:   src/components/Feed/feed.css 	modified:   src/components/RightBar/RightBar.jsx 	modified:   src/components/RightBar/rightbar.css

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -2,7 +2,28 @@ import './rightbar.css'
 
 const RightBar = () => {
   return (
-    <div className='rightbar'>RightBar</div>
+     <div className="rightbar">
+        <div className="rightbar-wrapper">
+            <h3 className="rightbar-title">Online Friends  </h3>
+                <ul className="rightbar-friend-list">
+                    <li className="rightbar-friend">
+                        <div className="rightbar-profile-img-container">
+                            <img src="public\assets\person\bala.jpg" alt="" className="rightbar-img" />
+                            <span className="rightbar-online"></span>
+                        </div>
+                        <span className="rightbar-user-name">Bala</span>
+                    </li>
+                    <li className="rightbar-friend">
+                        <div className="rightbar-profile-img-container">
+                            <img src="public\assets\person\bala.jpg" alt="" className="rightbar-img" />
+                            <span className="rightbar-online"></span>
+                        </div>
+                        <span className="rightbar-user-name">Bala</span>
+                    </li>
+                    
+                </ul>
+        </div>
+     </div>
   )
 }
 
